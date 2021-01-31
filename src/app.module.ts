d2c872da62932944b2ef564d7467f742bb23c04e
Ivan Orekhov <makito82@gmail.com>
2021-01-31 22:30:42 +0300

    configure database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
-  imports: [WalletsModule],
+  imports: [
+    WalletsModule,
+    TypeOrmModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
