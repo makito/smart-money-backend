0e81adb19e936abdf55195d89127f0153487f2e7
Ivan Orekhov <makito82@gmail.com>
2021-01-31 22:32:04 +0300

    refactoring wallet service to use database

--- src/wallets/dto/update-wallet.dto.ts
+++ src/wallets/dto/update-wallet.dto.ts
@@ -1,4 +1,14 @@
+import { Exclude, Expose } from 'class-transformer';
+import { IsString, MaxLength, IsNumber } from 'class-validator';
+
+@Exclude()
 export class UpdateWalletDto {
+  @Expose()
+  @IsString()
+  @MaxLength(25)
   readonly name: string;
+
+  @Expose()
+  @IsNumber()
   readonly amount: number;
 }
--- src/wallets/main.service.ts
+++ src/wallets/main.service.ts
@@ -1,28 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { Repository } from 'typeorm';
+
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { Wallet } from './models/wallet.entity';
 
 @Injectable()
 export class MainService {
+
+  constructor(
+    @InjectRepository(Wallet)
+    private walletsRepository: Repository<Wallet>,
+  ) {}
   
-  async getAll(): Promise<string[]> {
-    return Promise.resolve([]);
+  getAll(): Promise<Wallet[]> {
+    return this.walletsRepository.find();
   }
 
-  getById(id: number) {
-    return id
+  getById(id: number): Promise<Wallet> {
+    return this.walletsRepository.findOne(id);
   }
 
-  create(data: CreateWalletDto) {
-    return data
+  async create(wallet: CreateWalletDto): Promise<Wallet> {
+    const wlt = plainToClass(CreateWalletDto, wallet);
+    const errors = await validate(wlt);
+    if (errors.length) {
+      throw new BadRequestException('Invalid wallet create data');
+    }
+    return this.walletsRepository.save(wallet);
   }
 
-  remove(id: number) {
-    return id;
+  async remove(id: number): Promise<void> {
+    await this.walletsRepository.delete(id);
   }
 
-  update(id: number, data: UpdateWalletDto) {
-    return data;
+  async update(id: number, data: UpdateWalletDto) {
+    const wlt = plainToClass(UpdateWalletDto, data);
+    const errors = await validate(wlt);
+    if (errors.length) {
+      throw new BadRequestException('Invalid wallet update data');
+    }
+    return (await this.walletsRepository.update({ id }, wlt)).generatedMaps;
   }
 
 }
